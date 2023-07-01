75341f853fd01f8e9c9588fe2e74813e2d4b91c3
VitaliKozhurov <vitalik24021992@gmail.com>
2023-07-01 23:04:33 +0300

    type for dispatch

--- src/type_thunk/thunk_type.ts
+++ src/type_thunk/thunk_type.ts
@@ -1,25 +1,25 @@
-import axios from "axios";
-import { type } from "os";
-import { Dispatch } from "redux";
-import { ThunkAction } from "redux-thunk";
+import axios from 'axios';
+import {type} from 'os';
+import {Dispatch} from 'redux';
+import {ThunkAction} from 'redux-thunk';
 
 // examples
 // В данном примере dispatch не типизируем и можем диспатчить что угодно
 const removeTodoListTC_1 = (todoListID: string) => (dispatch: any) => {
-    return axios.delete("example.url.com").then((res) => dispatch(1));
+    return axios.delete('example.url.com').then((res) => dispatch(1));
 };
 
 // Второй пример, достаем тип dispatch из redux !!!
 // тип по умолчанию такой объект {  type: T}
 const removeTodoListTC_2 = (todoListID: string) => (dispatch: Dispatch) => {
     return axios
-        .delete("example.url.com")
-        .then((res) => dispatch({ type: res }));
+        .delete('example.url.com')
+        .then((res) => dispatch({type: res}));
 };
 
 // Более конкретная типизация dispatch для конкретого action
 type ExampleType = {
-    type: "MY-TYPE";
+    type: 'MY-TYPE';
     payload: any;
 };
 
@@ -27,23 +27,23 @@ const removeTodoListTC_3 =
     (todoListID: string) => (dispatch: Dispatch<ExampleType>) => {
         return (
             axios
-                .delete("example.url.com")
+                .delete('example.url.com')
                 //.then((res) => dispatch({ type: any })); // будет ошибка
-                .then((res) => dispatch({ type: "MY-TYPE", payload: "123" }))
+                .then((res) => dispatch({type: 'MY-TYPE', payload: '123'}))
         );
     };
 
 // Более конкретная типизация dispatch для всех actions редьюсера
 type FirstActionType = {
-    type: "FIRST-ACTION-TYPE";
+    type: 'FIRST-ACTION-TYPE';
     payload: any;
 };
 type SecondActionType = {
-    type: "SECOND-ACTION-TYPE";
+    type: 'SECOND-ACTION-TYPE';
     payload: any;
 };
 type ThirdActionType = {
-    type: "THIRD-ACTION-TYPE";
+    type: 'THIRD-ACTION-TYPE';
     payload: any;
 };
 type ActionsType = FirstActionType | SecondActionType | ThirdActionType;
@@ -53,10 +53,10 @@ const removeTodoListTC_4 =
     (todoListID: string) => (dispatch: Dispatch<ActionsType>) => {
         return (
             axios
-                .delete("example.url.com")
+                .delete('example.url.com')
                 //.then((res) => dispatch({ type: any })); // будет ошибка
                 .then((res) =>
-                    dispatch({ type: "FIRST-ACTION-TYPE", payload: "123" })
+                    dispatch({type: 'FIRST-ACTION-TYPE', payload: '123'})
                 )
         );
     };
@@ -67,34 +67,83 @@ const removeTodoListTC_4 =
 const getTodoListTC_1 =
     (todoListID: string) => (dispatch: Dispatch<ActionsType>) => {
         return axios
-            .post("example.url.com" + todoListID)
+            .post('example.url.com' + todoListID)
             .then((res) =>
-                dispatch({ type: "SECOND-ACTION-TYPE", payload: res })
+                dispatch({type: 'SECOND-ACTION-TYPE', payload: res})
             );
     };
 
-const addTodoListTC_1 = () => (dispatch: Dispatch<ActionsType>) => {
-    return axios
-        .post("example.url.com")
-        .then((res) => dispatch(getTodoListTC_1(res))); // ошибка нелбзя диспатчить функцию
-    // только объект с полем type
-};
+// const addTodoListTC_1 = () => (dispatch: Dispatch<ActionsType>) => {
+//     return axios
+//         .post('example.url.com')
+//         .then((res) => dispatch(getTodoListTC_1(res))); // ошибка нелбзя диспатчить функцию
+//     // только объект с полем type
+// };
+
+
 // type ThunkAction<ReturnType, State, ExtraThunkArg, BasicAction extends Action> = (dispatch: ThunkDispatch<State, ExtraThunkArg, BasicAction>, getState: () => State, extraArgument: ExtraThunkArg) => ReturnType;
 // ReturnType то что санка возвращает --- void (ничего)
-type AppWithThunType = ThunkAction<>;
-
+// State тип нашего стейта
+// Extra arguments -- unknown -- неизвестно
+// Basic action -- все экшены приложения
+// объекты с полем type
+type AppState = any;    // -- state app
+type AppWithThunkType = ThunkAction<void, AppState, unknown, ActionsType>;
+// Теперь типизируем только thunk creator
 const getTodoListTC_2 =
-    (todoListID: string) => (dispatch: Dispatch<ActionsType>) => {
+    (todoListID: string): AppWithThunkType => (dispatch) => {
         return axios
-            .post("example.url.com" + todoListID)
+            .post('example.url.com' + todoListID)
             .then((res) =>
-                dispatch({ type: "SECOND-ACTION-TYPE", payload: res })
+                dispatch({type: 'SECOND-ACTION-TYPE', payload: res})
             );
     };
 
-const addTodoListTC_2 = () => (dispatch: Dispatch<ActionsType>) => {
+const addTodoListTC_2 = (): AppWithThunkType => (dispatch) => {
     return axios
-        .post("example.url.com")
-        .then((res) => dispatch(getTodoListTC_2(res))); // ошибка нелбзя диспатчить функцию
+        .post('example.url.com')
+        .then((res) => dispatch(getTodoListTC_2('id_todo'))); // ошибка нелбзя диспатчить функцию
     // только объект с полем type
 };
+
+
+// ======================== //
+// type for redux-toolkit
+// типизация стейта выглядит следующим образом
+// type RootState = ReturnType<typeof store.getState>
+// type AppDispatch = typeof store.dispatch
+
+// кастомный dispatch с типизацией
+// const AppDispatch = ()=>useDispatch<AppDispatch>()
+
+// кастомный селект с типизацией
+// TypeUseSelectorHook взят из react-redux
+// const useAppSelector:TypeUseSelectorHook<RootState> = useSelector
+
+
+// ========== REDUX ================ /
+
+// type RootStateType = ReturnType<typeof store.getState>
+
+
+const getTodosReduxTC_1 = (todoListID: string) => (dispatch: Dispatch/*(из redux)*/) => {
+    axios.get('example.com')
+        .then(res => dispatch({type: res}))
+}
+
+// типизация для санок когда надо диспатчить санку
+// type AppThunk = ThunkAction<void, AppState, unknown, ActionsType>;
+
+// типизация dispatch
+// здесь мы используем ThunkDispatch из redux-thunk
+// указываем туда тип стейта, неизвестный тип
+// и тип экшенов для редьюсеров
+// type AppDispatch = ThunkDispatch<RootState, unknown, ActionsType>
+
+
+
+
+
+
+
+
